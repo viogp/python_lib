86dbe767caecd53cb286f3a63227aa2e0f48436c
Violeta GP <violetagp@protonmail.com>
2020-02-14 15:26:11 +0000

    get_snap admits now ranges of redshift

--- bahamas.py
+++ bahamas.py
@@ -14,29 +14,28 @@ tblz = 'snap_z.txt'
 
 def table_z_sn(sim,outdir):
     output = outdir+sim+'/'+tblz
+    path = dirbahamas+sim+'/Data/EagleSubGroups_5r200/'
+    dirs = glob.glob(path+'groups_*')
 
-    path = dirbahamas+sim+'/Data/EagleSubGroups_5r200/'                               
-    dirs = glob.glob(path+'groups_*')                                                   
-                                                                                        
-    # Initialize arrays for z and sn                                                    
-    zzs = np.zeros(shape=len(dirs)) ; zzs.fill(-999.)                                   
-    sns = np.zeros(shape=len(dirs), dtype=int)                                          
-                                                                                        
-    for ii, dir in enumerate(dirs):                                                     
-        snap = dir.split('groups_0')[1]                                                 
-        sns[ii] = float(snap)                                                           
-                                                                                        
-        infile = dir+'/eagle_subfind_tab_0'+snap+'.0.hdf5'                              
-        f = h5py.File(infile, 'r')                                                      
-                                                                                        
-        header = f['Header']                                                            
-        zzs[ii] = header.attrs['Redshift']                                              
-        #zz = '{0:.2f}'.format(header.attrs['Redshift'])                                
-        f.close()                                                                       
-                                                                                        
-    # Sort by redshift                                                                  
-    ind = np.argsort(zzs)                                                               
-    zz = zzs[ind]                                                                       
+    # Initialize arrays for z and sn
+    zzs = np.zeros(shape=len(dirs)) ; zzs.fill(-999.)
+    sns = np.zeros(shape=len(dirs), dtype=int)
+
+    for ii, dir in enumerate(dirs):
+        snap = dir.split('groups_0')[1]
+        sns[ii] = float(snap)
+
+        infile = dir+'/eagle_subfind_tab_0'+snap+'.0.hdf5'
+        f = h5py.File(infile, 'r')
+        header = f['Header']
+
+        zzs[ii] = header.attrs['Redshift']
+        #zz = '{0:.2f}'.format(header.attrs['Redshift'])
+        f.close()
+
+    # Sort by redshift
+    ind = np.argsort(zzs)
+    zz = zzs[ind]
     sn = sns[ind]
 
     # Write list to file                                                                
@@ -97,7 +96,7 @@ def get_z(snap,sim,outdir):
         zz = zzs[ind][0]
         return zz
 
-def get_snap(zz,sim,outdir):
+def get_snap(zz,zmin,zmax,sim,outdir):
     """
     Get the closest snapshot given a redshift and
     a simulation name
@@ -106,6 +105,10 @@ def get_snap(zz,sim,outdir):
     -----------
     zz : float
         Redshift
+    zmin : float
+        Minimum redsfhit to look for the sanpshot
+    zmax : float
+        Maximum redsfhit to look for the sanpshot
     sim : string
         Name of the Bahamas simulation
     outdir : string
@@ -121,7 +124,7 @@ def get_snap(zz,sim,outdir):
     Examples
     ---------
     >>> import bahamas as b
-    >>> b.get_snap(3.2,'AGN_TUNED_nu0_L100N256_WMAP9')
+    >>> b.get_snap(3.2,3.,3.4,'AGN_TUNED_nu0_L100N256_WMAP9')
     >>>
     """
 
@@ -132,30 +135,31 @@ def get_snap(zz,sim,outdir):
         tablez = table_z_sn(sim,outdir)
 
     # Read the table:
-    zzs, x = np.loadtxt(tablez, unpack=True)
-    sns = np.asarray(x,dtype=int)
+    zzs, lsns = np.loadtxt(tablez, unpack=True)
 
     # Case of having a single snapshot:
     if (not hasattr(zzs, "__len__")):
-        if (np.abs(zz-zzs)<0.25):
-            return sns
+        if (zmin < zzs and zzs < zmax):
+            return lsns,zzs
         else:
-            print('WARNING: {} far from single z found in {}'.format(zz,zzs,tablez))
+            print('WARNING: {} far from single {} found in {}'.format(zz,zzs,tablez))
             return -999.,-999.
-    
+    sns = np.asarray(lsns,dtype=int)
+
     # Find the closest redshift
-    if (zz < zzs[0]):
-        print('WARNING: {} below {}, min. z found in {}'.format(zz,zzs[0],tablez))
-        if (zz<0):
-            return -999.,-999.
-        else: 
-            return sns[0],zzs[0]
-    elif(zz > zzs[-1]):
-        print('WARNING: {} above {}, max. z found in {}'.format(zz,zzs[-1],tablez))
-        return sns[-1],zzs[-1]
+    if ((zz < zzs[0] and zmax < zzs[0]) or (zz<0.)):
+        print('WARNING: {}<{}, min. z found in {}'.format(zz,zzs[0],tablez))
+        return -999.,-999.
+    elif(zz > zzs[-1] and zmin > zzs[-1]):
+        print('WARNING: {}>{}, max. z found in {}'.format(zz,zzs[-1],tablez))
+        return -999.,-999.
     else:
         idx = (np.abs(zzs - zz)).argmin()
-        return sns[idx],zzs[idx]
+        if (zmin < zzs[idx] and zzs[idx] < zmax):
+            return sns[idx],zzs[idx]
+        else:
+            print('WARNING: z={} outside range {}<z<{}, {}'.format(zzs[idx],zmin,zmax,tablez))
+            return -999.,-999.
 
 if __name__== "__main__":
     print(get_z('-1','AGN_TUNED_nu0_L100N256_WMAP9'))
