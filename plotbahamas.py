0a3d67d801beb326181100c387c7793017ff5ce4
Violeta Gonzalez-Perez <violegp@gmail.com>
2020-05-19 12:25:50 +0100

    Start working on mf_sims

--- plotbahamas.py
+++ plotbahamas.py
@@ -79,8 +79,10 @@ def wctime(sims,labels,env,dirplot=None,zrange=None):
 
     Parameters
     -----------
-    sim : list of strings
+    sims : list of strings
         Array with the names of the simulation
+    labels : list of strings
+        Array with the labels to be used
     env : string
         ari or cosma, to use the adecuate paths
     dirplot : string
@@ -112,7 +114,7 @@ def wctime(sims,labels,env,dirplot=None,zrange=None):
     ax = plt.subplot()
     cols = get_distinct(lensims)
     xtit = 'Age (Gyr)'
-    ytit = 'Wallclock time (s)'
+    ytit = 'Wallclock time (h)'
     ax.set_xlabel(xtit) ; ax.set_ylabel(ytit)
 
     # Loop over all the simulations to be compared
@@ -128,7 +130,6 @@ def wctime(sims,labels,env,dirplot=None,zrange=None):
 
         # Read file with wall clock information
         infof = path+'info.txt'
-        start_time = time.time()
         redshift = [] ; wctime = []
         with open(infof, "r") as ff:
             for line in ff:
@@ -145,6 +146,7 @@ def wctime(sims,labels,env,dirplot=None,zrange=None):
                         
         wc0 = wctime[0]
         wctime = [x - wc0 for x in wctime]
+        wctime = [x/3600. for x in wctime]  # From s to hours
 
         # Find lowest redshift
         if (lowestz > float(redshift[-1])): lowestz = float(redshift[-1])
@@ -156,7 +158,7 @@ def wctime(sims,labels,env,dirplot=None,zrange=None):
         age = [cosmo.age_of_universe(x) for x in redshift]
 
         # Plot wall clock time vs age
-        ax.plot(age,wctime,color=cols[ii],label=labels[ii])
+        ax.plot(age,wctime,c=cols[ii],label=labels[ii])
 
     # If specified, set z range using the last loaded cosmology
     if zrange is not None:
@@ -204,8 +206,10 @@ def cputime(sims,labels,env,dirplot=None,zrange=None):
 
     Parameters
     -----------
-    sim : list of strings
+    sims : list of strings
         Array with the names of the simulation
+    labels : list of strings
+        Array with the labels to be used
     env : string
         ari or cosma, to use the adecuate paths
     dirplot : string
@@ -265,7 +269,6 @@ def cputime(sims,labels,env,dirplot=None,zrange=None):
 
         # Read file with cpu information
         infof = path+'cpu.txt'
-        start_time = time.time()
         icount = 0
         with open(infof, "r") as ff:
             for line in ff:
@@ -349,18 +352,182 @@ def cputime(sims,labels,env,dirplot=None,zrange=None):
     fig.savefig(plotf)
 
     return plotf
+
+def mf_sims(redshift,massdef,sims,labels,env,dirplot=None,Testing=False):
+    """
+    Compare the halo mass function of different simulations at a given z
+
+    Parameters
+    -----------
+    redshift : float
+        Redshift to make the plot
+    massdef : string
+        Name of the mass definition to be used
+    sims : list of strings
+        Array with the names of the simulation
+    labels : list of strings
+        Array with the labels to be used
+    env : string
+        ari or cosma, to use the adecuate paths
+    dirplot : string
+        Path to plots
+    Testing : boolean
+        True or False for testing with few subfiles
+
+    Returns
+    -----
+    plotf : string
+        Path to plot
+
+    ExamplesL050N256/WMAP9_PMGRID1024/Sims/ws_96_84_mu_7_76_dT_7_71_n_24_BH_DensTh_m_2_76_nt
+    ---------
+    >>> import plotbahamas as pb
+    >>> pb.mf_sims(7.,'Group_M_Mean200',['L050N256/WMAP9_PMGRID1024/Sims/ws_96_84_mu_7_76_dT_7_71_n_24_BH_DensTh_m_2_76_ntask128'],['ntask128'],'cosma')
+    """ 
+
+    # Check that the size of the arrays for the simulations and labels is the same
+    lensims = len(sims)
+    if (len(labels) != lensims):
+        print('WARNING! Labels array does not match Sims array')
+        # Generate labels
+        labels = [x.split('/')[-1] for x in sims]
+
+    # Bins in halo mass
+    mmin = 9. ; mmax = 16. ; dm = 0.1
+    edges = np.array(np.arange(mmin,mmax,dm))
+    mhist = edges[1:]-0.5*dm  #; print("mhist={}".format(mhist)) 
+
+    # The subfiles to loop over
+    nvols = 'All'
+    if Testing: nvols = 2
+
+    # Set up plot variables
+    fig = plt.figure()
+    ax = plt.subplot()
+    cols = get_distinct(lensims)
+    xtit = '${\\rm log}_{10}(\\rm{M/M_{\odot}}h^{-1})$' 
+    ytit = '${\\rm log}_{10}(\Phi/ Mpc^{-3}h^3 {\\rm dlog}_{10}M)$'
+    ax.set_xlabel(xtit) ; ax.set_ylabel(ytit)
+
+    xmin = 10. ; xmax = 16.
+    ymin = -6.5 ; ymax = 0.
+    ax.set_xlim(xmin,xmax) ;  ax.set_ylim(ymin,ymax) 
+
+    # Loop over all the simulations to be compared
+    lowestz = 999
+    for ii, sim in enumerate(sims):
+        # Simulation input
+        if (env == 'ari'):
+            dirb = b.dirbahamasari
+            path = dirb+sim+'/Data/EagleSubGroups_5r200/'
+        elif (env == 'cosma'):
+            dirb = b.dirbahamascosma
+        path = dirb+sim+'/data/'
+#
+#        # Initialize lists
+#        redshift = [] 
+#        #times = [[] for i in range(len(props))]
+#        percentages = [[] for i in range(len(props))]
+#
+#        # Read file with cpu information
+#        infof = path+'cpu.txt'
+
+#        icount = 0
+#        with open(infof, "r") as ff:
+#            for line in ff:
+#                if line.strip():
+#                    word = line.split()[0]
+#                    if (word == 'Step'):
+#                        s1 = line.split('z:')[1]
+#                        redshift.append(float(s1.split('CPUs')[0]))
+#                    elif (word in props):
+#                        ind = props.index(word)
+#                        #s1 = float(line.split()[1])
+#                        #times[ind].append(s1)
+#
+#                        s1 = line.split()[2]
+#                        s2 = float(s1.split('%')[0])
+#                        percentages[ind].append(s2)
+#
+#        # Find lowest redshift
+#        if (lowestz > float(redshift[-1])): lowestz = float(redshift[-1])
+#        if (lowestz > zs0[0]): lowestz = zs0[0]
+#
+#        # Set the cosmology for this simulation and calculate the ages
+#        omega0, omegab, lambda0, h0 = b.get_cosmology(sim,env)
+#        cosmo.set_cosmology(omega0=omega0,omegab=omegab,lambda0=lambda0,h0=h0,
+#                            universe="Flat",include_radiation=False)
+#        age = [cosmo.age_of_universe(x) for x in redshift]
+#
+#        # Plot properties
+#        for jj, jprop in enumerate(props):
+#            y = percentages[jj]
+#            l1, = ax.plot(age,y,color=cols[ii],linestyle=ls[jj])
+#
+#            if (ii==0):
+#                plot_lines.append(l1)
+#        plot_colors.append(l1)
+#
+#    # If specified, set z range using the last loaded cosmology
+#    if zrange is not None:
+#        if (len(zrange) == 2):            
+#            lowestz = zrange[0]
+#            xmax = cosmo.age_of_universe(lowestz)
+#            xmin = cosmo.age_of_universe(zrange[1])
+#            ax.set_xlim(xmin,xmax)
+#        else:
+#            print('WARNING (plotbahamas): zrange = [zmin,zmax]')
+#
+#    # Legend
+#    legend1 = ax.legend(plot_lines, props, loc=2)
+#    legend1.draw_frame(False)
+#    for h in legend1.legendHandles:
+#        h.set_color('k')
+#    plt.gca().add_artist(legend1)
+#
+#    leg = ax.legend(plot_colors, labels, loc=1,
+#                    handlelength=0, handletextpad=0)
+#    leg.draw_frame(False)
+#    for ii,text in enumerate(leg.get_texts()):
+#        text.set_color(cols[ii])
+#    for item in leg.legendHandles:
+#        item.set_visible(False)
+#
+#    # Top axis with redshift
+#    xmin, xmax = ax.get_xlim()
+#    zs, zticks = get_zticks(lowestz,xmin,xmax)
+#    axz = ax.twiny()
+#    axz.minorticks_off()
+#    axz.set_xticks(zticks)
+#    axz.set_xticklabels(['{:g}'.format(x) for x in zs])
+#    axz.set_xlim(xmin, xmax)
+#    axz.set_xlabel('z')
+#
+    # Path to plot
+    if (dirplot == None):
+        dirp = dirb+'plots/'+sim+'/'
+    else:
+        dirp = dirplot+sim+'/'
+
+    if (not os.path.exists(dirp)):
+        os.makedirs(dirp)
+    plotf = dirp+'mf_sims_z'+str(redshift)+'_'+massdef+'.pdf'
+    print(plotf) #here
+    fig.savefig(plotf)
+
+    return plotf
     
 
 if __name__== "__main__":
     env = 'cosma'
 
     if (env == 'cosma'):
-        sim1 = 'L050N256/WMAP9/Sims/ws_96_84_mu_7_76_dT_7_71_n_24_BH_DensTh_m_2_76_tmax0_01_ntask128'
-        sim2 = 'L050N256/WMAP9/Sims/ws_96_84_mu_7_76_dT_7_71_n_24_BH_DensTh_m_2_76_tmax0_047619_ntask128'
-        sim3 = 'L050N256/WMAP9/Sims/ws_96_84_mu_7_76_dT_7_71_n_24_BH_DensTh_m_2_76_tmax0_125_ntask128' 
+        sim1 = 'L050N256/WMAP9/Sims/ws_96_84_mu_7_76_dT_7_71_n_24_BH_DensTh_m_2_76_ntask128'
+        sim2 = 'L050N256/WMAP9_PMGRID1024/Sims/ws_96_84_mu_7_76_dT_7_71_n_24_BH_DensTh_m_2_76_ntask128'
 
-        sims = [sim1]#, sim2, sim3]
-        labels = ['t$_{max}$=0.01']#,'t$_{max}$=0.05','t$_{max}$=0.125']
-        print(wctime(sims,labels,'cosma'))
+        sims = [sim1, sim2]
+        labels = ['PMGRID = 512','PMGRID = 1024']
+        #print(wctime(sims,labels,'cosma'))
         #print(cputime(sims,labels,'cosma'))
+        print(mf_sims(7.,'Group_M_Mean200',sims,labels,'cosma',Testing=True))
 
